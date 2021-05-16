207e954deb0cff7a6bbd7df2da561c27829a11c9
Jiří Kuhn <jkuhn@vendavo.com>
2021-05-16 23:33:14 +0200

    Fast login (forgotten class to commit)

--- app/src/main/java/board/user/MemoryUserRepository.java
+++ app/src/main/java/board/user/MemoryUserRepository.java
@@ -1,21 +1,24 @@
 package board.user;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Hard coded list of users.
  */
 public class MemoryUserRepository implements UserRepository {
-    private Map<String, User> users;
+    private final Map<String, User> users;
 
     public MemoryUserRepository() {
-        users = new HashMap<>();
+        users = new LinkedHashMap<>();
 
         // create users
         addUser("jirka",  "Jirka");
         addUser("vojta",  "Vojta");
-        addUser("betka",  "Bětka");
+        addUser("betka",  "Bětka Ku");
         addUser("kaja",  "Kája");
         addUser("sarka",  "Šárka");
 
@@ -24,8 +27,9 @@ public class MemoryUserRepository implements UserRepository {
         addUser("aja",  "Ája");
         addUser("kami",  "Kamča");
         addUser("karol",  "Karolínka");
-        addUser("paja", "Pája");
+        addUser("betka2", "Bětka Kr");
         addUser("jolca", "Jolča");
+        addUser("paja", "Pája");
     }
 
     private void addUser(String id, String name) {
@@ -35,5 +39,10 @@ public class MemoryUserRepository implements UserRepository {
     @Override
     public User findUserByLoginName(String loginName) {
         return users.get(loginName);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(this.users.values());
     }
 }
